4260bc6659c69eda377b7f6baf5916a52726837e
francis85a <franperezcid85@gmail.com>
2026-04-22 12:45:39 +0200

    feat(ScoreCard): Inicializar clase ScoreCard

---
+++ src/main/java/edu/teamrocket/frank_brunos_boxing/ScoreCard.java
@@ -0,0 +1,14 @@
+package edu.teamrocket.frank_brunos_boxing;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class ScoreCard {
+    private final String color;
+    private String redCorner = "";
+    private String blueCorner = "";
+    private String[] judgeScoreCard;
+}
