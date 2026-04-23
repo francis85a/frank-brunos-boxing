e4f22b8b03ac22aa9d9b1e4627bb64970f8ed292
francis85a <franperezcid85@gmail.com>
2026-04-23 10:06:01 +0200

    refactor(Round factory): Refactorizar round factory con switch

--- src/main/java/edu/teamrocket/frank_brunos_boxing/RoundFactory.java
+++ src/main/java/edu/teamrocket/frank_brunos_boxing/RoundFactory.java
@@ -3,22 +3,14 @@ package edu.teamrocket.frank_brunos_boxing;
 public class RoundFactory {
     public static Round getRound(String roundScore) {
 
-    if (roundScore == null) {
+    if (roundScore == null) 
         return null;
-    }
-    if(roundScore.equalsIgnoreCase("10 - 9")
-            || roundScore.equalsIgnoreCase("9 - 10")) {
-            return new RegularRound(roundScore);
-    }
-    else if(roundScore.equalsIgnoreCase("10 - 8")
-            || roundScore.equalsIgnoreCase("8 - 10")) {
-            return new KnockdownRound(roundScore);
-
-    } 
-    else if(roundScore.equalsIgnoreCase("1, 8 - 10")
-            || roundScore.equalsIgnoreCase("10 - 8 ,1")) {
-            return new PointsDeducted(roundScore);
-    }
-    return null;
+    
+    return switch (roundScore.replace(" ", "")) {
+        case "10-9", "9-10" -> new RegularRound(roundScore);
+        case "10-8", "8-10" -> new KnockdownRound(roundScore);
+        case "1,8-10", "10-8,1" -> new PointsDeducted(roundScore);
+        default -> null;
+        };
     }
 }
